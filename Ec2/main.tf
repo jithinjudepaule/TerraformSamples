69b0ae9891780b7d98f7a44f874d2bab9d55213d
Jithin Jude Paul <jithinjudepaule@gmail.com>
2022-02-13 12:43:41 +0530

    Add files via upload

---
+++ Ec2/main.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-033b95fb8079dc481"
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
